62a3c3f4c71f0847c5f9a94402fd6f1c6d31d375
Dwight <dwight@Dwights-MacBook-Pro.local>
2021-05-28 08:57:59 -0700

    new branch

--- public/popup/popup.js
+++ public/popup/popup.js
@@ -1,3 +1,5 @@
+const { computeHeadingLevel } = require("@testing-library/dom");
+
 document.getElementById("toggle").addEventListener("click", tabInfo);
 
 
@@ -21,7 +23,6 @@ function tabInfo(){
         //console.log(safeStatus);
         //document.getElementById("response").innerHTML=safeStatus + "<br>";
 
-        
         // // Get URL length
         // var url_len = activeTab.url.length;
 
@@ -35,22 +36,38 @@ function tabInfo(){
         // document.writeln("Top-level Domain: " + tld);
         // document.writeln("Protocol: " + protocol);
     
+        var approvedWebsites = new Array(3);
+        approvedWebsites[0] = "Website 1";
+        approvedWebsites[1] = "Website 2";
+        approvedWebsites[2] = "Website 3";
+
+        
+
+
         //check if the on button for the extension is switched to on
         if (document.getElementById("toggle").checked == false){
             document.getElementById("trackers").innerHTML="Extension disabled. Not counting trackers";
-            document.getElementById("intensityButtons").innerHTML='';
+            document.getElementById("intensityButtons");
         }
         // Get number of cookies
         else {
             chrome.cookies.getAll({"url":activeTab.url}, function (cookie){
             //document.writeln("Cookies detected: " + cookie.length);
-            document.getElementById("trackers").innerHTML="Number of trackers on this website: " + cookie.length + '<br>';
-            if (cookie.length > 0) {
-                console.log(cookie);
-                console.log(cookie[0]["domain"]);
-            }
-
+              document.getElementById("trackers").innerHTML="Number of trackers on this website: " + cookie.length + '<br>';
+              if (cookie.length > 0) {
+                  console.log(cookie);
+                  console.log(cookie[0]["domain"]);
+              }
             });
+          document.getElementById("intensityQuestion");
+          document.getElementById("intensityButtons");
+          let list = document.getElementById("approvedWebsiteList");
+          let approvedWebsites = ["Website0", "Website1", "Website2"];
+          approvedWebsites.forEach((item) => {
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(item))
+            list.appendChild(li);
+          })
             //buttons for different extension intensities
             // document.getElementById("intensityButtons").innerHTML= '<p> How intense would you like the extension to work?</p> <br>' + '<input type="radio" name="favourite_colour"' +
             // 'value="low" checked> Low - only warns when there are 30 or more cookies <br>' + 
