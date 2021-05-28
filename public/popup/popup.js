d345ffb5adf44341ab6482a1a1584aa463d0ca91
David Suzuki <David@suzuki-aloha.com>
2021-05-27 23:25:54 -0700

    Enable toggle switch; shows number of trackers; intensity buttsons; sends url to server and gets response

--- public/popup/popup.js
+++ public/popup/popup.js
@@ -1,3 +1,7 @@
+document.getElementById("toggle").addEventListener("click", tabInfo);
+
+
+
 function tabInfo(){
    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -5,28 +9,86 @@ function tabInfo(){
         // Should only be one tab so can just grab the first one
         var activeTab = tabs[0];
 
-        // Make URL object
+        // // Make URL object
         var url = new URL(activeTab.url);
-
-        // Get URL length
-        var url_len = activeTab.url.length;
-
-        // Get top-level domain (com, net, org, etc.)
-        var tld = url.hostname.split(".").slice(-1)[0];
-        
-        // Get protocol (1 for https, 0 for http)
-        var protocol = (url.protocol=="https:") ? 1 : 0;
-
-        document.writeln("URL length: " + url_len);
-        document.writeln("Top-level Domain: " + tld);
-        document.writeln("Protocol: " + protocol);
-
-        // Get number of cookies
-        chrome.cookies.getAll({"url":activeTab.url}, function (cookie){
-            document.writeln("Cookies detected: " + cookie.length);
+        console.log(activeTab.url);
+        document.getElementById("url").innerHTML=activeTab.url;
+        //send to server the url of the website
+        var safeStatus = postData('http://143.198.97.103:8080/', { answer: activeTab.url })
+        .then(data => {
+            document.getElementById("response").innerHTML=data.Resp + "<br>";; // JSON data parsed by `data.json()` call
         });
+        //console.log(safeStatus);
+        //document.getElementById("response").innerHTML=safeStatus + "<br>";
+
+        
+        // // Get URL length
+        // var url_len = activeTab.url.length;
+
+        // // // Get top-level domain (com, net, org, etc.)
+        // var tld = url.hostname.split(".").slice(-1)[0];
+        
+        // // // Get protocol (1 for https, 0 for http)
+        // var protocol = (url.protocol=="https:") ? 1 : 0;
+
+        // document.writeln("URL length: " + url_len);
+        // document.writeln("Top-level Domain: " + tld);
+        // document.writeln("Protocol: " + protocol);
+    
+        //check if the on button for the extension is switched to on
+        if (document.getElementById("toggle").checked == false){
+            document.getElementById("trackers").innerHTML="Extension disabled. Not counting trackers";
+            document.getElementById("intensityButtons").innerHTML='';
+        }
+        // Get number of cookies
+        else {
+            chrome.cookies.getAll({"url":activeTab.url}, function (cookie){
+            //document.writeln("Cookies detected: " + cookie.length);
+            document.getElementById("trackers").innerHTML="Number of trackers on this website: " + cookie.length + '<br>';
+            if (cookie.length > 0) {
+                console.log(cookie);
+                console.log(cookie[0]["domain"]);
+            }
+
+            });
+            //buttons for different extension intensities
+            // document.getElementById("intensityButtons").innerHTML= '<p> How intense would you like the extension to work?</p> <br>' + '<input type="radio" name="favourite_colour"' +
+            // 'value="low" checked> Low - only warns when there are 30 or more cookies <br>' + 
+            // '<input type="radio" name="favourite_colour"' + 
+            // 'value="medium"> Medium - only warns when there are 20 or more cookies<br>' + 
+            // '<input type="radio" name="favourite_colour"' +
+            // 'value="high"> High - only warns when there are 10 or more cookies <br>';
+        }
+        
 
      });
 
 }
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      //body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(data)
+    });
+
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+  
+//   postData('https://143.198.97.103/getdata', { answer: activeTab.url })
+//     .then(data => {
+//       console.log(data); // JSON data parsed by `data.json()` call
+//     });
 window.onload=tabInfo;
+
